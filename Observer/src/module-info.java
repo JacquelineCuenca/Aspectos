b415c70c6b0a78451887da8ccfa3b77ef2e2e628
ValeriaQuimi <vdquimi@espol.edu.ec>
2025-06-10 21:18:39 -0500

    Creando ventana

--- Observer/src/module-info.java
+++ Observer/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Observer {
+	requires java.desktop;
 }
---
+++ Observer/src/tarea/Ventana.java
@@ -0,0 +1,29 @@
+package tarea;
+
+import javax.swing.*; // Importar los componentes gráficos 
+import java.awt.*;    // Importar clases para manejar colores y layouts
+import java.awt.event.ActionEvent; // Importar clases para manejar eventos 
+
+public class Ventana {
+
+	//metodo principal
+	public static void main(String[] args) {
+		//llamar al metodo para crear interfaz
+		 SwingUtilities.invokeLater(Ventana::createAndShowGUI);
+	}
+	
+	// Método que construye y muestra la ventana 
+    private static void createAndShowGUI() {
+        // Crear la ventana principal 
+        JFrame frame = new JFrame("Ventana de Colores");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        frame.setSize(600, 400); // Definir el tamaño de la ventana 
+
+        // Crear un panel donde se colocarán los botones
+        JPanel panel = new JPanel();
+        frame.add(panel); // Agregar el panel a la ventana
+
+        
+    }
+	
+}
